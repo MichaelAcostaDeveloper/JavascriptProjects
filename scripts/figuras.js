606101f0143f95f7537be24c52bd8e23bdfe2d24
MichaelAcostaDeveloper <acostamichael80desarrollo@gmail.com>
2022-02-07 23:43:48 -0500

    Codigo listo para determinar si existe o no un triangulo y calculo de su area

--- scripts/figuras.js
+++ scripts/figuras.js
@@ -41,23 +41,19 @@ function perimetroCircunferencia(radio){
     return diametro(radio)*Math.PI
 }
 console.groupEnd();
-
+//En esta seccion se establece las funciones para la app de figuras
 //Aqui se ubican las funciones para el html de figuras
 function calcularAreaCuadrado(){
     const input=document.getElementById("InputCuadrado");
     const valor=input.value;
-
     const area=areaCuadrado(valor);
-
     const resultado=document.getElementById("result");
     resultado.innerHTML="The surface is: "+area+" cm²";
 }
 function calcularPerimetroCuadrado(){
     const input=document.getElementById("InputCuadrado");
     const valor=input.value;
-
     const perimetro=perimetroCuadrado(valor);
-
     const resultado=document.getElementById("result");
     resultado.innerHTML="The perimeter is: "+perimetro+" cm";
 }
@@ -67,9 +63,7 @@ function calcularAreaRectangulo(){
     const valorA=inputA.value;
     const inputB=document.getElementById("InputRectanguloB");
     const valorB=inputB.value;
-
     const area=areaRectangulo(valorA,valorB);
-
     const resultado=document.getElementById("resultRectangle");
     resultado.innerHTML="The surface is: "+area+" cm²";
 }
@@ -79,14 +73,12 @@ function calcularPerimetroRectangulo(){
     const inputB=document.getElementById("InputRectanguloB");
     const valorB=inputB.value;
     const perimetro=perimetroRectangulo(valorA,valorB);
-    
     const resultado=document.getElementById("resultRectangle");
     resultado.innerHTML="The perimeter is: "+perimetro+" cm";
 }
 function circulo(){
     const radio=document.getElementById("InputRadio")
     const r=Number(radio.value);
-
     const area=areaCircunferencia(r);
     const areaR=Math.round(area)
     const resultado=document.getElementById("resultCircunf")
@@ -96,9 +88,77 @@ function circulo(){
 function circunf(){
     const radio=document.getElementById("InputRadio")
     const r=Number(radio.value);
-
     const perimetro=perimetroCircunferencia(r);
     const perimetroR=Math.round(perimetro)
     const resultado=document.getElementById("resultCircunf")
     resultado.innerHTML="The surfaces is: "+perimetroR+" cm"
+}
+//con esta funcion se verifica que existe el triangulo
+function verificarTriangulo(a,b,c){
+    if ((a+b>c) & (a+c>b) & (b+c>a)) {
+        console.log("SI EXISTE EL TRIANGULO")
+        calculo_area_rectangulo(a,b,c)
+        return true
+    } else{
+        console.log("NO EXISTE EL TRIANGULO")
+        return false
+    }
+}
+
+
+//con esta funcion se verifica que es un triangulo rectangulo
+function calculo_area_rectangulo(a,b,c){
+    if(Math.pow(a,2)===Math.pow(b,2)+Math.pow(c,2)){
+        console.log(`Es triangulo rectangulo y la superficie es: ${b*c/2}`)
+        return b*c/2//area del triangulo
+    }else if(Math.pow(b,2)===Math.pow(a,2)+Math.pow(c,2)){
+        console.log(`Es triangulo rectangulo y la superficie es: ${a*c/2}`)
+        return a*c/2//area del triangulo
+    }else if(Math.pow(c,2)===Math.pow(b,2)+Math.pow(a,2)){
+        console.log(`Es triangulo rectangulo y la superficie es: ${b*a/2}`)
+        return a*b/2//area del triangulo
+    }else{
+        calculo_area_oblicuangulo(a,b,c)
+    }
+}
+
+function calculo_area_oblicuangulo(a,b,c){
+    var B=Math.acos((Math.pow(a,2)+Math.pow(c,2)-Math.pow(b,2))/(2*a*c))
+    var A=Math.acos((Math.pow(b,2)+Math.pow(c,2)-Math.pow(a,2))/(2*b*c))
+    var C=Math.acos((Math.pow(a,2)+Math.pow(b,2)-Math.pow(c,2))/(2*a*b))
+
+    //evaluacion de un lado mayor y el resto diferentes o evaluacion de dos lados iguales pero mayores al tercero
+    if((((a>b)&(a>c))&((b===c)||(c>b)||(b>c)))||(((a<b)&(a<c))&((b===c)||(c>b)||(b>c)))){
+        altura=b*Math.sin(C)
+        console.log(`Es triangulo oblicuangulo y la superficie es: ${a*altura/2}`)
+        return a*altura/2
+        }
+        else if((((b>c)&(b>a))&((a===c)||(c>a)||(a>c)))||((((b<c)&(b<a))&((a===c)||(c>a)||(a>c))))){
+        altura=a*Math.sin(C)
+        console.log(`Es triangulo oblicuangulo y la superficie es: ${b*altura/2}`)
+        return b*altura/2
+        }
+        else if((((c>a)&(c>b))&((b===a)||(a>b)||(b>a)))||(((c<a)&(c<b))&((b===a)||(a>b)||(b>a)))){
+        altura=b*Math.sin(A)
+        console.log(`Es triangulo oblicuangulo y la superficie es: ${c*altura/2}`)
+        return c*altura/2
+        }
+        //evaluacion de los 3 lados iguales
+        else if((a===b) & (b===c)& (c===a)){
+            altura=a*Math.sin(B)
+            console.log(`Es triangulo oblicuangulo y la superficie es: ${a*altura/2}`)
+            return a*altura/2
+       }
+
+        }
+    
+function checkingTriangle(){
+    var lado_a=document.getElementById("lado_a");
+    var lado_a=Number(lado_a.value);
+    var lado_b=document.getElementById("lado_b");
+    var lado_b=Number(lado_b.value);
+    var lado_c=document.getElementById("lado_c");
+    var lado_c=Number(lado_c.value);
+
+    const res=verificarTriangulo(lado_a,lado_b,lado_c)
 }
